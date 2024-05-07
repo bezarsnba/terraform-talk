5550541ed5cc714c99b7d49c374b65891f7735c0
Beza <bezaleel.silva@jusbrasil.com.br>
2024-05-07 18:23:36 -0300

    update config

--- main.tf
+++ main.tf
@@ -3,9 +3,3 @@ resource "local_file" "test" {
   filename        = "${path.module}/hello.txt"
   file_permission = "0666"
 }
-resource "local_file" "test" {
-  content         = "Hello, World!"
-  filename        = "${path.module}/hello.txt"
-  file_permission = "0666"
-}
-
